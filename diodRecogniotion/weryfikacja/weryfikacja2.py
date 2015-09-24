dac55fca2c8fa2884cc475e8974279f60de2f21b
megierek <wegierek.maciej@gmail.com>
2015-09-24 16:31:49 +0200

    testy matlaba

--- diodRecogniotion/weryfikacja/weryfikacja2.py
+++ diodRecogniotion/weryfikacja/weryfikacja2.py
@@ -4,15 +4,8 @@ import math
 from itertools import izip
 import transformations
 
-#homogMatrix = np.zeros(shape=(4,4))
-
-
-#homogMatrix = np.array([[homogVector[0],homogVector[1],homogVector[2],homogVector[3]],[homogVector[4],homogVector[5],homogVector[6],homogVector[7]],[homogVector[8],homogVector[9],homogVector[10],homogVector[11]],[homogVector[12],homogVector[13],homogVector[14],homogVector[15]]])
-
-
 def liczenieOdleglosci():
 	global homogMatrix
-	#global odlegloscPozycji
 	homogMatrix = np.array([[0,0,0,2],[0,3,4,4],[5,5,6,3],[0,0,0,1]])
 	odlegloscPozycji = np.sqrt(pow(homogMatrix[0][3],2) + pow(homogMatrix[0][3],2) + pow(homogMatrix[0][3],2))
 	print homogMatrix[0][3]
@@ -27,74 +20,116 @@ def czytanieMacierzyT01():
 	wyniki = 0
 	wynikQuaternion = []
 	wynikPrzesuniecie = []
-	#f = open("wyniki2_kropki_biale.txt")
 
-	with open("/home/mwegiere/ws_irp6/mwegiere_ws/src/serwo/diodRecogniotion/weryfikacja/wyniki2_czerwona.txt") as textfile1, open("/home/mwegiere/ws_irp6/mwegiere_ws/src/serwo/diodRecogniotion/weryfikacja/wyniki2_zielona.txt") as textfile2:
-	#with open("wyniki2_kropki_biale.txt") as textfile1, open("wyniki2_kropki_czarne.txt") as textfile2:
+	diody = 1
+	szachownica = 0
+	if diody:
+		plik1 = "wyniki2_czerwona.txt"
+		plik2 = "wyniki2.txt"
+
+	if szachownica:
+		plik1 = "wyniki2_czerwona.txt"
+		plik2 = "wyniki2_zielona.txt"
+
+	if diody:
+		diody_obroty = open("../../wyniki/diody_obroty", "wb")
+		diody_przesuniecie = open("../../wyniki/diody_przesuniecie", "wb")
+	 	diody_przesuniecie_x = open("../../wyniki/diody_przesuniecie_x", "wb")
+		diody_przesuniecie_y = open("../../wyniki/diody_przesuniecie_y", "wb")
+		diody_przesuniecie_z = open("../../wyniki/diody_przesuniecie_z", "wb")
+
+	if szachownica:
+		szachownica_obroty = open("../../wyniki/szachownica_obroty", "wb")
+		szachownica_przesuniecie = open("../../wyniki/szachownica_przesuniecie", "wb")
+	 	szachownica_przesuniecie_x = open("../../wyniki/szachownica_przesuniecie_x", "wb")
+		szachownica_przesuniecie_y = open("../../wyniki/szachownica_przesuniecie_y", "wb")
+		szachownica_przesuniecie_z = open("../../wyniki/szachownica_przesuniecie_z", "wb")
+	
+	with open("/home/mwegiere/ws_irp6/mwegiere_ws/src/serwo/diodRecogniotion/weryfikacja/"+plik1) as textfile1, open("/home/mwegiere/ws_irp6/mwegiere_ws/src/serwo/diodRecogniotion/weryfikacja/"+plik2) as textfile2:	
 		for x, y in izip(textfile1, textfile2):
 			homogVectorX[licznikWierszyX%16] = float(x)
 			homogVectorY[licznikWierszyY%16] = float(y)
 			licznikWierszyX = licznikWierszyX + 1
 			licznikWierszyY = licznikWierszyY + 1
 			licznikWierszy = licznikWierszy + 1
-			#wczytal juz cala tablice
 			
 			if (licznikWierszy%16 == 15 ):
 				homogVectorX[15] = 1.0
 				homogMatrixX = np.matrix([[homogVectorX[0],homogVectorX[1],homogVectorX[2],homogVectorX[3]],[homogVectorX[4],homogVectorX[5],homogVectorX[6],homogVectorX[7]],[homogVectorX[8],homogVectorX[9],homogVectorX[10],homogVectorX[11]],[homogVectorX[12],homogVectorX[13],homogVectorX[14],homogVectorX[15]]])
 				homogVectorY[15] = 1.0
 				homogMatrixY = np.matrix([[homogVectorY[0],homogVectorY[1],homogVectorY[2],homogVectorY[3]],[homogVectorY[4],homogVectorY[5],homogVectorY[6],homogVectorY[7]],[homogVectorY[8],homogVectorY[9],homogVectorY[10],homogVectorY[11]],[homogVectorY[12],homogVectorY[13],homogVectorY[14],homogVectorY[15]]])
-				Taa = np.linalg.inv(homogMatrixX)*homogMatrixY
-				odlegloscPozycji = np.sqrt(pow(Taa.item(0,3),2) + pow(Taa.item(1,3),2) + pow(Taa.item(2,3),2))
-				#print "a"
-				#print Taa.item(0,3)
-				#print Taa.item(1,3)
-				#print Taa.item(2,3)
 				
+				Taa = homogMatrixX*np.linalg.inv(homogMatrixY)
+	
+				odlegloscPozycji = np.sqrt(pow(Taa.item(0,3),2) + pow(Taa.item(1,3),2) + pow(Taa.item(2,3),2))
+				if diody:
+					print "kupa"
+					diody_przesuniecie.write(str(odlegloscPozycji)+"\n")
+					diody_przesuniecie_x.write(str(Taa.item(0,3))+"\n")
+					diody_przesuniecie_y.write(str(Taa.item(1,3))+"\n")
+					diody_przesuniecie_z.write(str(Taa.item(2,3))+"\n")
+
+				if szachownica:
+					print "dupa"
+					szachownica_przesuniecie.write(str(odlegloscPozycji)+"\n")
+					szachownica_przesuniecie_x.write(str(Taa.item(0,3))+"\n")
+					szachownica_przesuniecie_y.write(str(Taa.item(1,3))+"\n")
+					szachownica_przesuniecie_z.write(str(Taa.item(2,3))+"\n")
+						
 				wynikPrzesuniecie.append(odlegloscPozycji)
 				quaternion = transformations.quaternion_from_matrix([[Taa.item(0,0),Taa.item(1,0),Taa.item(2,0)],[Taa.item(1,0),Taa.item(1,1),Taa.item(1,2)],[Taa.item(2,0),Taa.item(2,1),Taa.item(2,2)]])
 
-				#print np.sqrt(2*(1-np.linalg.norm(quaternion*quaternion)))
-				wynikQuaternion.append(np.sqrt(2*(1-np.linalg.norm(quaternion*quaternion))))
-				
-				
-					
-				
-				#print quaternion*quaternion
+				odlegloscOrientacji = np.sqrt(2*(1-np.linalg.norm(quaternion*quaternion)))
+				wynikQuaternion.append(odlegloscOrientacji)
+
+				if diody:
+					diody_obroty.write(str(odlegloscOrientacji)+"\n")
+
+				if szachownica:
+					szachownica_obroty.write(str(odlegloscOrientacji)+"\n")
+
 				licznikMacierzy = licznikMacierzy + 1
-		#print wynikQuaternion
 		mediana = 0
 		for item in wynikQuaternion:
 			mediana = mediana + float(item)
-			print item
+			#print item
 		mediana = mediana/licznikMacierzy
-		#print mediana
 
 		odchylenie = 0
 		for item in wynikQuaternion:
 			odchylenie = odchylenie + pow((float(item) - mediana),2)
 		odchylenie = odchylenie / 3
-		print "odchylenie standardowe dla obrotu:  "
-		print np.sqrt(odchylenie)
+		#print "odchylenie standardowe dla obrotu:  "
+		#print np.sqrt(odchylenie)
 
 		mediana = 0
 		for item in wynikPrzesuniecie:
 			mediana = mediana + float(item)
-                        print item
+                        #print item
 		mediana = mediana/licznikMacierzy
-		#print mediana
 
 		odchylenie = 0
 		for item in wynikPrzesuniecie:
 			odchylenie = odchylenie + pow((float(item) - mediana),2)
 		odchylenie = odchylenie / 3
-		print "odchylenie standardowe dla przesuniecia:  "
-		print np.sqrt(odchylenie)
-			
-					
+		#print "odchylenie standardowe dla przesuniecia:  "
+		#print np.sqrt(odchylenie)
+
+		if diody:
+			diody_obroty.close()
+			diody_przesuniecie.close()
+	 		diody_przesuniecie_x.close()
+			diody_przesuniecie_y.close()
+			diody_przesuniecie_z.close()
+
+		if szachownica:
+			szachownica_obroty.close()
+			szachownica_przesuniecie.close()
+	 		szachownica_przesuniecie_x.close()
+			szachownica_przesuniecie_y.close()
+			szachownica_przesuniecie_z.close()		
+								
 if __name__ == '__main__':
 	#liczenieOdleglosci()
 	czytanieMacierzyT01()
 
-
-	
---
+++ wyniki/test.m
@@ -0,0 +1,58 @@
+fileID = fopen('diody_obroty','r');
+formatSpec = '%f';
+diody_obroty = fscanf(fileID,formatSpec);
+
+fileID = fopen('diody_przesuniecie','r');
+formatSpec = '%f';
+diody_przesuniecie = fscanf(fileID,formatSpec);
+
+fileID = fopen('diody_przesuniecie_x','r');
+formatSpec = '%f';
+diody_przesuniecie_x = fscanf(fileID,formatSpec);
+
+fileID = fopen('diody_przesuniecie_y','r');
+formatSpec = '%f';
+diody_przesuniecie_y = fscanf(fileID,formatSpec);
+
+fileID = fopen('diody_przesuniecie_z','r');
+formatSpec = '%f';
+diody_przesuniecie_z = fscanf(fileID,formatSpec);
+
+fileID = fopen('szachownica_obroty','r');
+formatSpec = '%f';
+szachownica_obroty = fscanf(fileID,formatSpec);
+
+fileID = fopen('szachownica_przesuniecie','r');
+formatSpec = '%f';
+szachownica_przesuniecie = fscanf(fileID,formatSpec);
+
+fileID = fopen('szachownica_przesuniecie_x','r');
+formatSpec = '%f';
+szachownica_przesuniecie_x = fscanf(fileID,formatSpec);
+
+fileID = fopen('szachownica_przesuniecie_y','r');
+formatSpec = '%f';
+szachownica_przesuniecie_y = fscanf(fileID,formatSpec);
+
+fileID = fopen('szachownica_przesuniecie_z','r');
+formatSpec = '%f';
+szachownica_przesuniecie_z = fscanf(fileID,formatSpec);
+
+skala = 1:70
+
+figure(1);
+clf(1);
+hold on
+
+plot (skala, diody_obroty, '.r')
+plot (skala, szachownica_obroty, '*r')
+plot (skala, diody_przesuniecie, '.g')
+plot (skala, szachownica_przesuniecie, '*g')
+plot (skala, diody_przesuniecie_x, '.b')
+plot (skala, szachownica_przesuniecie_x, '*b')
+plot (skala, diody_przesuniecie_y, '.c')
+plot (skala, szachownica_przesuniecie_y, '*c')
+plot (skala, diody_przesuniecie_z, '.m')
+plot (skala, szachownica_przesuniecie_z, '*m')
+legend('DIODYobroty','obroty', 'DIODYprzesuniecie', 'przesuniecie', 'DIODYprzesuniecie x','przesuniecie_x', 'DIODYprzesuniecie_y','przesuniecie_y', 'DIODYprzesuniecie_z', 'przesuniecie_z')
+hold off
