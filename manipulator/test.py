3fbfb882cbb29769e4326b7c145a69bf3595b346
megierek <wegierek.maciej@gmail.com>
2015-10-16 16:24:17 +0200

    porzadki cd

--- manipulator/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import transformations
-
-
-if __name__ == '__main__':
-
-	print transformations.quaternion_from_euler(-0.01745329251,0,0)
--- trackedObject/testDiodyWorld (kopia).m
+++
@@ -1,4 +0,0 @@
-fileID = fopen('diody','r');
-formatSpec = '%f';
-diody_obroty = fscanf(fileID,formatSpec);
-
