e55aa8a7df58a9b8c82fb08ab36b9f894b1e24fd
megierek <wegierek.maciej@gmail.com>
2015-11-05 20:39:24 +0100

    simulink

--- tmp/serwoInfo.py
+++ tmp/serwoInfo.py
@@ -14,6 +14,10 @@ import geometry_msgs.msg
 
 import matrixOperations
 
+#konwencja
+#T_AB - macierz przeksztacenia jednorodnego pomiedzy ukladem B, a ukladem A, czyli pozycja ukladu A w ukladzie B
+#w mojej notacji A stoi na dole, a B stoi na gorze
+
 class serwoInfo:
 
   def __init__(self): 
@@ -34,7 +38,7 @@ class serwoInfo:
     self.nsec = 0
 
     #definicjie macierzy
-    self.T_GC = 0
+    self.T_GC = np.empty([4, 4])
     self.T_BC_koniec = np.empty([4, 4])
     self.T_GB_init = np.empty([4, 4])
     self.T_GC_koniec = np.empty([4, 4])
@@ -59,18 +63,33 @@ class serwoInfo:
         self.tmp_sec_kon = self.out_time_sec_kon
         self.tmp_nsec_kon = self.out_time_sec_kon
 
-
-        self.listener.waitForTransform('/p_c_optical_frame', '/world', rospy.Time(0), rospy.Duration(10))
-    	(trans,rot) = self.listener.lookupTransform('/p_c_optical_frame', '/world', rospy.Time(0))
-    	print trans
+	#pobranie pozycji ... w ukladzie ... z tf-a
+        self.listener.waitForTransform('/p_c_optical_frame', '/pl_base', rospy.Time(0), rospy.Duration(10))
+    	#(trans,rot) = self.listener.lookupTransform('/p_c_optical_frame', '/world', rospy.Time(0))
+        (trans,rot) = self.listener.lookupTransform('/p_c_optical_frame', '/pl_base', rospy.Time(0))
+    	#bezposredni wynik dzialania tf-a
+	print trans
         print rot
+
+	#liczenie roznicy czasu
     	self.sec = self.out_time_sec_kon - self.out_time_sec_pocz
         self.nsec = self.out_time_nsec_kon - self.out_time_nsec_pocz
         print "czas sekundy i nanosekundy od zrobienia zdjecia do momentu wyslania info z SolvePnP"
         print self.sec
         print self.nsec
+
+	#T_GC_koniec - macierz przeksztacenia jednorodnego pomiedzy ukladem C, a ukladem G, czyli pozycja ukladu G w ukladzie C
         self.T_GC = matrixOperations.translation_from_vector(self.matrix)
-	self.T_BC_koniec = matrixOperations.euler_matrix_from_quaternion(rot, trans)
+	print "wynik dzialania discode, sprawdz co to jest"
+	print self.T_GC
+	print np.linalg.inv(self.T_GC)
+	print "\n"
+
+	#T_CB_koniec - macierz przeksztacenia jednorodnego pomiedzy ukladem B, a ukladem C, czyli pozycja ukladu C w ukladzie B
+	self.T_CB_koniec = matrixOperations.euler_matrix_from_quaternion(rot, trans)
+	print "wynik dzialania tf-a, sprawdz co to jest"
+	print self.T_BC_koniec
+
 	if (self.inicjalizacja == 0):
           self.T_GB_init = self.T_GC * self.T_BC_koniec
           self.inicjalizacja = 1
