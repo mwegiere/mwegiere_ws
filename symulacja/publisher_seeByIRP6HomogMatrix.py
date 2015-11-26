b088f0542523de9e3f20bf7c4e29882d0cbfac2d
megierek <wegierek.maciej@gmail.com>
2015-11-26 10:51:19 +0100

    cleanup

--- symulacja/publisher_seeByIRP6HomogMatrix.py
+++ symulacja/publisher_seeByIRP6HomogMatrix.py
@@ -2,17 +2,27 @@
 # license removed for brevity
 import rospy
 import math
-from std_msgs.msg import Float32
+from serwo.msg import SerwoInfo
 
 def talker():
-    pub = rospy.Publisher('seeByIRP6HomogMatrix', Float32, queue_size=10)
+    pub = rospy.Publisher('seeByIRP6HomogMatrix', SerwoInfo, queue_size=10)
     rospy.init_node('seeByIRP6HomogMatrix', anonymous=True)
+    msg = SerwoInfo()
+    matrix = []
 
     #w rzeczywistosci obiekt poruszasza sie z taka czestotliwoscia
-    rate = rospy.Rate(1) 
+    rate = rospy.Rate(40) 
     while not rospy.is_shutdown():
         y = math.sin(rospy.get_time()/5)
-        pub.publish(y)
+        y = 2.2 + y/4
+        matrix = [1,0,0,0,0,1,0,y,0,0,1,0,0,0,0,1]
+        msg.matrix = matrix;
+        msg.found = 1;
+        msg.out_time_nsec_pocz = 0;
+        msg.out_time_sec_pocz = 0;
+        msg.out_time_nsec_kon = 0;
+        msg.out_time_sec_kon = 0;
+        pub.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
--- tasmociag/generowanie_trajektorii.py
+++ tasmociag/generowanie_trajektorii.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#generacja pliku cvs do poruszania tasmociagiem
 file = open("fun.csv" ,"w")
 file.write("aaa")
 
